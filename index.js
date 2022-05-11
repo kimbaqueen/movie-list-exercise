da44e25f6d7a35b13fe61a8dc5bf170b40e0f41c
Kim Starr <98854861+kimbaqueen@users.noreply.github.com>
2022-05-10 20:52:04 -0600

    core lab completed

---
+++ index.js
@@ -0,0 +1,41 @@
+// console.log("Hello world");
+
+document.querySelector("form").addEventListener("submit", addMovie);
+const message = document.querySelector('#message');
+
+function addMovie(event) {
+    event.preventDefault();
+    let inputField = document.querySelector('input');
+
+    const movie = document.createElement('li');
+
+    const movieTitle = document.createElement('span');
+    movieTitle.textContent = inputField.value;
+    movieTitle.addEventListener("click", crossOffMovie);
+    movie.appendChild(movieTitle);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "x";
+    deleteBtn.addEventListener("click", deleteMovie);
+    movie.appendChild(deleteBtn);
+
+    const list = document.querySelector("ul");
+    list.appendChild(movie);
+
+    inputField.value = '';
+}
+
+function deleteMovie(event) {
+    event.target.parentNode.remove();
+    message.textContent = 'Movie Deleted';
+}
+
+function crossOffMovie(event) {
+    event.target.classList.toggle('checked');
+
+    if (event.target.classList.contains('checked') === true) {
+        message.textContent = 'Movie Watched';
+    } else {
+        message.textContent = 'Movie added back';
+    }
+}
