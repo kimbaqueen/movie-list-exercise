6ee7a8d18b0f3faafc0908c7ed61d6bd1e45af17
Kim Starr <98854861+kimbaqueen@users.noreply.github.com>
2022-05-10 21:21:13 -0600

    intermediate completed

--- index.js
+++ index.js
@@ -27,15 +27,27 @@ function addMovie(event) {
 
 function deleteMovie(event) {
     event.target.parentNode.remove();
-    message.textContent = 'Movie Deleted';
+
+    // message.textContent = 'Movie Deleted';
+    message.textContent = `${event.target.parentNode.firstChild.textContent} deleted`;
 }
 
 function crossOffMovie(event) {
     event.target.classList.toggle('checked');
 
     if (event.target.classList.contains('checked') === true) {
-        message.textContent = 'Movie Watched';
+        // message.textContent = 'Movie Watched';
+        message.textContent = `${event.target.textContent} watched`;
     } else {
-        message.textContent = 'Movie added back';
+        // message.textContent = 'Movie added back';
+        message.textContent = `${event.target.textContent} added back`;
     }
+    revealMessage();
+}
+
+function revealMessage() {
+    message.classList.remove('hide');
+    setTimeout(() => {
+        message.classList.add('hide')
+    }, 1000);
 }
